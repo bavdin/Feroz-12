251e360d932ea112be35d6f44344675f94aec919
Mukku Rajput <rajputmukku02@gmail.com>
2026-01-29 14:09:01 +0000

    Edit 0_ARIF-PAIR-2.js

--- models/commands/0_ARIF-PAIR-2.js
+++ models/commands/0_ARIF-PAIR-2.js
@@ -1,109 +1,130 @@
 module.exports.config = {
   name: "pair2",
-  version: "1.0.0",
+  version: "4.4.0",
   hasPermssion: 0,
   credits: "ARIF BABU",
-  description: "MADE BY ARIF BABU",
+  description: "Pair DP (Only DP with background image)",
+  usePrefix: true,
   commandCategory: "PAIR 2",
-  usages: "",
+  usages: "[reply | mention | random]",
+  cooldowns: 5,
   dependencies: {
-        "axios": "",
-        "fs-extra": ""
-  },
-  cooldowns: 0
+    "axios": "",
+    "fs-extra": "",
+    "canvas": ""
+  }
+};
+
+module.exports.run = async function ({ api, event, Users }) {
+  const { createCanvas, loadImage } = require("canvas");
+  const fs = require("fs-extra");
+  const axios = require("axios");
+
+  const cache = __dirname + "/cache/";
+  if (!fs.existsSync(cache)) fs.mkdirSync(cache, { recursive: true });
+
+  const outPath = cache + "pair.png";
+  const a1 = cache + "a1.png";
+  const a2 = cache + "a2.png";
+  const bgPath = cache + "bg.jpg";
+
+  const id1 = event.senderID;
+  const name1 = await Users.getNameUser(id1);
+  let id2;
+
+  if (event.messageReply?.senderID) {
+    id2 = event.messageReply.senderID;
+  } else if (Object.keys(event.mentions || {}).length === 1) {
+    id2 = Object.keys(event.mentions)[0];
+  } else {
+    const info = await api.getThreadInfo(event.threadID);
+    const mem = info.participantIDs.filter(
+      i => i !== id1 && i !== api.getCurrentUserID()
+    );
+    id2 = mem[Math.floor(Math.random() * mem.length)];
+  }
+
+  const name2 = await Users.getNameUser(id2);
+
+  const token = "6628568379|c1e620fa708a1d5696fb991c1bde5662";
+
+  // ===== AVATARS =====
+  const av1 = await axios.get(
+    `https://graph.facebook.com/${id1}/picture?type=large&access_token=${token}`,
+    { responseType: "arraybuffer" }
+  );
+  fs.writeFileSync(a1, av1.data);
+
+  const av2 = await axios.get(
+    `https://graph.facebook.com/${id2}/picture?type=large&access_token=${token}`,
+    { responseType: "arraybuffer" }
+  );
+  fs.writeFileSync(a2, av2.data);
+
+  // ===== BACKGROUND IMAGE =====
+  const bgImg = await axios.get(
+    "https://i.ibb.co/Sw7JN4q8/fb3bb0ed54c5.jpg",
+    { responseType: "arraybuffer" }
+  );
+  fs.writeFileSync(bgPath, bgImg.data);
+
+  // ===== CANVAS =====
+  const W = 1920;
+  const H = 1080;
+  const canvas = createCanvas(W, H);
+  const ctx = canvas.getContext("2d");
+
+  const bg = await loadImage(bgPath);
+  ctx.drawImage(bg, 0, 0, W, H);
+
+  const img1 = await loadImage(a1);
+  const img2 = await loadImage(a2);
+
+  // 🔒 FIXED POSITION (NO y PROBLEM)
+  const r = 220;
+  const leftX = 480;
+  const rightX = 1440;
+  const fixedY = 540;
+
+  drawCircle(ctx, img1, leftX, fixedY, r);
+  drawCircle(ctx, img2, rightX, fixedY, r);
+
+  ring(ctx, leftX, fixedY, r + 10);
+  ring(ctx, rightX, fixedY, r + 10);
+
+  fs.writeFileSync(outPath, canvas.toBuffer("image/png"));
+
+  fs.unlinkSync(a1);
+  fs.unlinkSync(a2);
+  fs.unlinkSync(bgPath);
+
+  return api.sendMessage(
+    {
+      body: `${name1} ❤️ ${name2}`,
+      mentions: [{ id: id2, tag: name2 }],
+      attachment: fs.createReadStream(outPath)
+    },
+    event.threadID,
+    () => fs.unlinkSync(outPath),
+    event.messageID
+  );
+};
+
+// ===== FUNCTIONS =====
+function drawCircle(ctx, img, x, y, r) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(img, x - r, y - r, r * 2, r * 2);
+  ctx.restore();
 }
 
-module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
-  const { loadImage, createCanvas } = require("canvas");
-  const fs = global.nodemodule["fs-extra"];
-  const axios = global.nodemodule["axios"];
-  let pathImg = __dirname + "/cache/background.png";
-  let pathAvt1 = __dirname + "/cache/Avtmot.png";
-  let pathAvt2 = __dirname + "/cache/Avthai.png";
-
-  var id1 = event.senderID;
-  var name1 = await Users.getNameUser(id1);
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
-  var all = ThreadInfo.userInfo
-  for (let c of all) {
-    if (c.id == id1) var gender1 = c.gender;
-  };
-  const botID = api.getCurrentUserID();
-  let ungvien = [];
-  if(gender1 == "FEMALE"){
-    for (let u of all) {
-      if (u.gender == "MALE") {
-      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
-      }
-    }
-  }
-  else if(gender1 == "MALE"){
-    for (let u of all) {
-      if (u.gender == "FEMALE") {
-      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
-      }
-    }
-  }
-  else {
-  for (let u of all) {
-      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
-    }
-  }
-  var id2 = ungvien[Math.floor(Math.random() * ungvien.length)];
-  var name2 = await Users.getNameUser(id2);
-  var rd1 = Math.floor(Math.random() * 100) + 1;
-  var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
-  var rd2 = cc[Math.floor(Math.random() * cc.length)];
-  var djtme = [`${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd2}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`];
-
-  var tile = djtme[Math.floor(Math.random() * djtme.length)];
-
-  var background = [
-  "https://i.ibb.co/bgdL7TJS/96ef57bc9680.jpg",
-  ];
-  var rd = background[Math.floor(Math.random() * background.length)];
-
-  let getAvtmot = (
-    await axios.get(
-      `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
-
-  let getAvthai = (
-    await axios.get(
-      `https://graph.facebook.com/${id2}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(pathAvt2, Buffer.from(getAvthai, "utf-8"));
-
-  let getbackground = (
-    await axios.get(`${rd}`, {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
-
-  let baseImage = await loadImage(pathImg);
-  let baseAvt1 = await loadImage(pathAvt1);
-  let baseAvt2 = await loadImage(pathAvt2);
-  let canvas = createCanvas(baseImage.width, baseImage.height);
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvt1, 230, 250, 240, 327);
-  ctx.drawImage(baseAvt2, 725, 245, 240, 330);
-  const imageBuffer = canvas.toBuffer();
-  fs.writeFileSync(pathImg, imageBuffer);
-  fs.removeSync(pathAvt1);
-  fs.removeSync(pathAvt2);
-  return api.sendMessage({ body: `🍫${name1}🍫\n🍫${name2}🍫\n✧═══•❁『${tile}%』❁•═══✧`,
-            mentions: [{
-          tag: `${name2}`,
-          id: id2
-        }], attachment: fs.createReadStream(pathImg) },
-      event.threadID,
-      () => fs.unlinkSync(pathImg),
-      event.messageID);
-  }
+function ring(ctx, x, y, r) {
+  ctx.strokeStyle = "#ffffff";
+  ctx.lineWidth = 18;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.stroke();
+}
